6cc5bb328f890aaf9317e32a8bbe629ddb0da253
sandeepshetty <sandeep.shetty@gmail.com>
2010-05-24 01:03:27 +0530

    Reformatted the route conf files. Don't ask, cause I can explain but you won't understand :)

--- route/default_routes.conf.php
+++ route/default_routes.conf.php
@@ -1,37 +1,74 @@
 <?php
 
-	$routes = array (
+	$routes = array
+	(
 
-		array('method'=>'GET',
-		      'path'=>'/[{handler}/]',
-		      'query'=>true,
-		      'defaults'=>array('handler'=>'',
-		                        'func'=>'query')),
+		array
+		(
+			'method'=>'GET',
+			'path'=>'/[{handler}/]',
+			'query'=>true,
+			'defaults'=>array
+			(
+				'handler'=>'',
+				'func'=>'query'
+			)
+		),
 
-		array('method'=>'GET',
-		      'path'=>'/[{handler}/]',
-		      'defaults'=>array('handler'=>'',
-		                        'func'=>'home')),
+		array
+		(
+			'method'=>'GET',
+			'path'=>'/[{handler}/]',
+			'defaults'=>array
+			(
+				'handler'=>'',
+				'func'=>'home'
+			)
+		),
 
-		array('method'=>'GET',
-		      'path'=>'/[{handler}/][{id:any}]',
-		      'defaults'=>array('handler'=>'',
-		                        'func'=>'show')),
+		array
+		(
+			'method'=>'GET',
+			'path'=>'/[{handler}/][{id:any}]',
+			'defaults'=>array
+			(
+				'handler'=>'',
+				'func'=>'show'
+			)
+		),
 
-		array('method'=>'PUT',
-		      'path'=>'/[{handler}/][{id:any}]',
-		      'defaults'=>array('handler'=>'',
-		                        'func'=>'save')),
+		array
+		(
+			'method'=>'PUT',
+			'path'=>'/[{handler}/][{id:any}]',
+			'defaults'=>array
+			(
+				'handler'=>'',
+				'func'=>'save'
+			)
+		),
 
-		array('method'=>'DELETE',
-		      'path'=>'/[{handler}/][{id:any}]',
-		      'defaults'=>array('handler'=>'',
-		                        'func'=>'delete')),
+		array
+		(
+			'method'=>'DELETE',
+			'path'=>'/[{handler}/][{id:any}]',
+			'defaults'=>array
+			(
+				'handler'=>'',
+				'func'=>'delete'
+			)
+		),
 
-		array('method'=>'POST',
-		      'path'=>'/[{handler}/]',
-		      'defaults'=>array('handler'=>'',
-		                        'func'=>post_action()))
+		array
+		(
+			'method'=>'POST',
+			'path'=>'/[{handler}/]',
+			'defaults'=>array
+			(
+				'handler'=>'',
+				'func'=>post_action()
+			)
+		)
 	);
 
 ?>
