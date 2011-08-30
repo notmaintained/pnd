b9d655ca1f5f37243a02d4116b687c124e2ca7bc
sandeepshetty <sandeep.shetty@gmail.com>
2011-08-31 00:19:00 +0530

    Made some code testable by avoiding playing around with references, removed some duplication, evolving standard to have a underscore suffix on all internal functions and requires() now triggers error with caller details.

--- bombay.php
+++ bombay.php
@@ -1,96 +1,61 @@
 <?php
 
-/* bombay.php
- *
- *
- *
- * function email($str){return preg_replace('/^(.*)\/(.*)/', '$2@$1', $str);}
- * Authors: Sandeep Shetty email('gmail.com/sandeep.shetty')
- *
- * Copyright (C) 2005 - date('Y') Collaboration Science,
- * http://collaborationscience.com/
- *
- * This file is part of Bombay.
- *
- * Bombay is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by the
- * Free Software Foundation; either version 2 of the License, or (at
- * your option) any later version.
- *
- * Bombay is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * General Public License for more details.
- *
- * To read the license please visit http://www.gnu.org/copyleft/gpl.html
- *
- *
- *-------10--------20--------30--------40--------50--------60---------72
- */
-
-
 	define('BOMBAY_DIR', dirname(__FILE__).DIRECTORY_SEPARATOR);
+
+
+ 	php_min_version_guard_('5.3.0');
+	fix_magic_quotes_gpc_();
+
+
+		function php_min_version_guard_($min_php_version)
+		{
+			$is_min_php_version = (function_exists('version_compare') and version_compare(PHP_VERSION,  $min_php_version, '>='));
+
+			if (!$is_min_php_version)
+			{
+				list($php_version, ) = explode('-', PHP_VERSION, 2);
+				trigger_error
+				(
+					"Currently running PHP $php_version. Bombay requires at least PHP $min_php_version. Error triggered by Bombay",
+					E_USER_ERROR
+				);
+			}
+		}
+
+		function fix_magic_quotes_gpc_()
+		{
+			if (get_magic_quotes_gpc())
+			{
+				list($_GET, $_POST, $_COOKIE, $_REQUEST) = array_stripslashes_(array($_GET, $_POST, $_COOKIE, $_REQUEST));
+			}
+		}
+
+			function array_stripslashes_($value)
+			{
+				if (is_array($value)) return array_map(__FUNCTION__, $value);
+				return stripslashes($value);
+			}
+
 
 
 	function requires()
 	{
 		$libraries = func_get_args();
+
 		foreach ($libraries as $library)
 		{
 			$library_file = BOMBAY_DIR.$library.DIRECTORY_SEPARATOR."$library.lib.php";
-			if (file_exists($library_file))
-			{
-				require_once $library_file;
-			}
-			else
-			{
-				trigger_error("Required library ($library) not found.", E_USER_ERROR);
-			}
+			if (!file_exists($library_file)) trigger_error_on_caller_("Requires non-existent library $library", E_USER_ERROR);
+			require_once $library_file;
 		}
 	}
-
-
- 	php_min_version_guard('4.3.0');
-	fix_magic_quotes_gpc();
-
-
-		function php_min_version_guard($min_php_version)
+		function trigger_error_on_caller_($message, $level)
 		{
-			$is_min_php_version = (function_exists('version_compare')
-								   and version_compare(PHP_VERSION,  $min_php_version, '>='));
-
-			if (!$is_min_php_version)
-			{
-				echo sprintf("ERROR: Older version of PHP. Current version of PHP is %s Please upgrade to PHP %s.",
-							 PHP_VERSION, $min_php_version);
-				exit;
-			}
+			$stacktrace = debug_backtrace();
+			$caller = $stacktrace[1];
+			$triggered_by = $stacktrace[0];
+			$me = __FUNCTION__;
+			trigger_error("$message in {$stacktrace[1]['file']} on line {$stacktrace[1]['line']}. Error triggered by {$stacktrace[1]['function']}() in {$stacktrace[0]['file']} on line {$stacktrace[0]['line']} from {$me}()", $level);
 		}
-
-
-		function fix_magic_quotes_gpc()
-		{
-			$funcname = 'array_stripslashes';
-
-			if (get_magic_quotes_gpc())
-			{
-				array_walk($_GET, $funcname);
-				array_walk($_POST, $funcname);
-				array_walk($_COOKIE, $funcname);
-				array_walk($_REQUEST, $funcname);
-			}
-		}
-
-			function array_stripslashes(&$value)
-			{
-				if (is_array($value))
-				{
-					array_walk($value, __FUNCTION__);
-				}
-				else
-				{
-					$value = stripslashes($value);
-				}
-			}
 
 ?>
--- bombay.test.php
+++ bombay.test.php
@@ -1,5 +1,14 @@
 <?php
 
-	// This is a hack to auto-include bombay.php so that requires() is available while running the tests.
+	// This file should exist even if it is empty. This is a hack to auto-include bombay.php so that requires() is available while running the tests.
+
+	function test_array_stripslashes_()
+	{
+		should_return
+		(
+			array("f'oo", "b'ar", array("fo'o", "b'ar")),
+			when_passed(array("f\\'oo", "b\\'ar", array("fo\\'o", "b\\'ar")))
+		);
+	}
 
 ?>
