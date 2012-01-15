59622734a6d3cd8697f4e064c2abb843ef8e4f23
sandeepshetty <sandeep.shetty@gmail.com>
2012-01-16 01:03:35 +0530

    Removed old tests...

--- request/tests/request.lib.test.php
+++ request/tests/request.lib.test.php
@@ -1,23 +1,10 @@
 <?php
 
-	function test_request_method_()
-	{
-		should_return('PUT', when_passed('put'));
-	}
-
-	function test_request_path_()
-	{
-		should_return('/foo', when_passed('foo'));
-		should_return('/foo', when_passed('/foo'));
-		should_return('/foo bar', when_passed('/foo bar'));
-	}
-
-
-	function test_request_body_()
+	function test_valid_body_()
 	{
         $data = 'hello world';
         should_return($data, when_passed($data));
-        should_return(NULL, when_passed(''));
+        should_return(NULL, when_passed(false));
 	}
 
 ?>
