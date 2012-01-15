a1367da66f405d3ced01b40e89692a7965c933ba
sandeepshetty <sandeep.shetty@gmail.com>
2012-01-16 02:15:29 +0530

    Removed comment header and reference to no longer existing function_exists_()

--- webserver/webserver.lib.php
+++ webserver/webserver.lib.php
@@ -1,31 +1,5 @@
 <?php
 
-/* webserver.lib.php
- *
- * function email($str){return preg_replace('/^(.*)\/(.*)/', '$2@$1', $str);}
- * Authors: Sandeep Shetty email('gmail.com/sandeep.shetty')
- *
- * Copyright (C) 2005 - date('Y') Collaboration Science,
- * http://collaborationscience.com/
- *
- * This file is part of Bombay.
- *
- * Bombay is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by the
- * Free Software Foundation; either version 2 of the License, or (at
- * your option) any later version.
- *
- * Bombay is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * General Public License for more details.
- *
- * To read the license please visit http://www.gnu.org/copyleft/gpl.html
- *
- *
- *-------10--------20--------30--------40--------50--------60---------72
- */
-
 
 	define('WEBSERVER', webserver(server_var('SERVER_SOFTWARE')));
 
@@ -75,7 +49,8 @@
 
 		function webserver_specific_func($webserver, $func)
 		{
-			if ($webserver_specific_func = function_exists_("{$webserver}_specific_{$func}"))
+			$webserver_specific_func = "{$webserver}_specific_{$func}";
+			if (function_exists($webserver_specific_func))
 			{
 				return $webserver_specific_func;
 			}
